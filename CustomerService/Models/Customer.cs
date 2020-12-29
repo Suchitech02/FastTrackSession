5f5bc3deda62a37ef731272e83a286cc1cb9358e
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-12-29 13:26:10 +0100

    Model Customer

---
+++ CustomerService/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace CustomerService.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
