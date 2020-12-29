16887b733b794ef5bb4af1ce119325fa2ab9b08c
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-12-29 13:39:09 +0100

    Fluent API for validating Model

--- CustomerService/Models/CustomerSchema.cs
+++ CustomerService/Models/CustomerSchema.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace CustomerService.Models
 {
-    public class CustomerSchema
+    public class CustomerSchema 
     {
-        
+        // Implementing Fluent API for validating the structure of Model (Avoid Data Annotation)
+        public CustomerSchema(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(t => t.ID);
+            builder.Property(t => t.Name).IsRequired();
+            builder.Property(t => t.Email);
+
+        }
     }
 }
